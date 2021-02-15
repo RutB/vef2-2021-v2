b838376334aff7f63d5f9c7aa554659aa21df24a
RutB <42755206+RutB@users.noreply.github.com>
2021-02-15 14:58:33 +0000

    upd

--- src/db.js
+++ src/db.js
@@ -64,26 +64,37 @@ import dotenv from 'dotenv';
 
 dotenv.config();
 
-// const {
-//   DATABASE_URL: connectionString,
-//   NODE_ENV: nodeEnv = 'development',
-// } = process.env;
+// // const {
+// //   DATABASE_URL: connectionString,
+// //   NODE_ENV: nodeEnv = 'development',
+// // } = process.env;
 
-const { DATABASE_URL: connectionString } = process.env;
-
-if (!connectionString) {
-  console.error("Vantar DATABASE_URL!");
-  process.exit(1);
-}
+// const { DATABASE_URL: connectionString } = process.env;
 
 // if (!connectionString) {
-//   console.error('Vantar DATABASE_URL');
+//   console.error("Vantar DATABASE_URL!");
 //   process.exit(1);
 // }
 
-//const ssl = nodeEnv !== 'development' ? { rejectUnauthorized: false } : false;
+// // if (!connectionString) {
+// //   console.error('Vantar DATABASE_URL');
+// //   process.exit(1);
+// // }
 
-const pool = new pg.Pool({ connectionString });
+// //const ssl = nodeEnv !== 'development' ? { rejectUnauthorized: false } : false;
+
+const {
+  DATABASE_URL: connectionString,
+  NODE_ENV: nodeEnv = 'development',
+} = process.env;
+
+if (!connectionString) {
+  console.error('Vantar DATABASE_URL');
+  process.exit(1);
+}
+const ssl = nodeEnv !== 'development' ? { rejectUnauthorized: false } : false;
+
+const pool = new pg.Pool({ connectionString, ssl });
 
 pool.on('error', (err) => {
   console.error('Unexpected error on idle client', err);
